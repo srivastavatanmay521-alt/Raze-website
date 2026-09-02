0845d9d1b631571e31231f0d231c5a6f8caa198d
srivastavatanmay521-alt <srivastavatanmay521@gmail.com>
2026-09-02 11:26:25 +0530

    Add files via upload

---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+import AdminPanel from "@/components/AdminPanel";
+
+export const metadata = {
+  robots: { index: false, follow: false },
+};
+
+export default function AdminPage() {
+  return <AdminPanel />;
+}
